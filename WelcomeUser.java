bcf72ea1d2a4aacf443d87ae994b6658513e1e13
Raunaq Ali <143211713+raunaqali@users.noreply.github.com>
2023-09-27 20:06:14 +0530

    Add files via upload

---
+++ WelcomeUser.java
@@ -0,0 +1,12 @@
+package com.telus.programmingfundamentals;
+
+public class WelcomeUser {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("Welcome to Java training");
+
+	}
+
+}
