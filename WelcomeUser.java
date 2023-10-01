b16a08174375942a16e13a964701fd88c06666d2
Raunaq Ali <143211713+raunaqali@users.noreply.github.com>
2023-10-01 23:54:11 +0530

    Add files via upload

--- WelcomeUser.java
+++ WelcomeUser.java
@@ -6,6 +6,19 @@ public class WelcomeUser {
 
 
 		System.out.println("Welcome to Java training");
+		
+		System.out.println("I am back again, inshallah i will succeed this time and complete the training");
+		
+		System.out.println("This time i will revise my previously studied topics");
+		
+		
+		System.out.println("This time i will write a framework from scratch");
+		
+		System.out.println("Inshallah");
+		
+		System.out.println("Inshallah");
+		
+		System.out.println("Inshallah");
 
 	}
 
